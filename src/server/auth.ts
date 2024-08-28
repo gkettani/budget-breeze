bacd85e4211fb96f8cee4ac27f1d3ae196ceacda
gkettani <ghalikettani01@gmail.com>
2024-08-28 14:13:40 +0100

    Update next auth session settings
    Change session maxAge from 30 to 3 days

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -47,6 +47,14 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   },
+  session: {
+    // Seconds - How long until an idle session expires and is no longer valid.
+    maxAge: 60 * 60 * 24 * 3, // 3 days
+
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    updateAge: 24 * 60 * 60, // 24 hours
+  },
   adapter: DrizzleAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
